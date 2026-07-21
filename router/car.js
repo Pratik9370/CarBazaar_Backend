f69e1fb56c962ebb7f12afedc2a16bf604526818
Pratik Jadhav <pratikvj9370@gmail.com>
2026-07-21 18:21:01 +0530

    checking 1.5

--- router/car.js
+++ router/car.js
@@ -49,7 +49,7 @@ router.post('/carList', async (req, res) => {
 
     const query = {};
 
-    if (price) query.Expected_price = { $lte: Number(price) };
+    
     if (fuel) query.Fuel_type = fuel;
     if (body) query.Body_type = body; // or {$regex: body, $options: 'i'} if needed
     if (transmission) query.Transmission = transmission;
