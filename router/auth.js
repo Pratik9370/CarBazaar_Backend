3b9fe9fb1daee2400a1ffa8e552fb329f7fa0873
Pratik Jadhav <pratikvj9370@gmail.com>
2026-08-19 15:13:00 +0530

    logout route created

--- router/auth.js
+++ router/auth.js
@@ -263,4 +263,20 @@ router.get('/verify', authenticateUser, (req, res) => {
     res.sendStatus(200);
 });
 
+router.post('/logout', authenticateUser, async (req, res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none'
+        });
+
+        res.status(200).json({
+            message: "Logged out successfully"
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 module.exports = router; 
